bf383576be981c57b34816f785c67d34f3a095ad
TrendingPC <jose@iiiiii>
2018-09-29 12:13:14 +0200

    version 2.0

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -16,8 +16,10 @@ public class Restaurante {
         private void mostrarMenu () {
             System.out.println("1- Crear producto");
             System.out.println("2- Crear pedido");
-            System.out.println("3- Añadir producto a pedido");
-            System.out.println("4- Salir");
+            System.out.println("3- Crear mesa");
+            System.out.println("4- Añadir producto a pedido");
+            System.out.println("5- Añadir pedido a mesa");
+            System.out.println("6- Salir");
             ejecutarOpcion();
 
         }
@@ -37,17 +39,24 @@ public class Restaurante {
                         crearPedido();
                         break;
                     case 3:
-                        añadirProductoPedido();
+                        crearMesa ();
+                        break;
                     case 4:
+                        añadirProductoPedido();
+                        break;
+                    case 5:
+                        añadirPedidoMesa ();
+                        break;
+                    case 6:
                         salir();
                         break;
                     default:
-                        System.out.println("opcion incorrecta, entre 1 y 4");
+                        System.out.println("opcion incorrecta, entre 1 y 5");
                         System.out.println("elija otra");
 
                 }
 
-            } while (opcion != 4);
+            } while (opcion != 6);
         }
 
         private void crearProducto () {
@@ -75,15 +84,88 @@ public class Restaurante {
         }
 
         private void crearPedido () {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/resturante?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+            System.out.print("id: ");
+            int id = sc.nextInt();
+            sc.nextLine();
+            System.out.print("total: ");
+            double total = sc.nextDouble();
+            sc.nextLine();
+            PreparedStatement ps = conn.prepareStatement("insert into pedido (id, total) values (?,?)");
+            ps.setInt(1,id);
+            ps.setDouble(2,total);
+            ps.executeUpdate();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
 
         }
 
         private void añadirProductoPedido () {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/resturante?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+            System.out.print("id_producto: ");
+            int id_producto = sc.nextInt();
+            sc.nextLine();
+            System.out.print("id_pedido: ");
+            int id_pedido = sc.nextInt();
+            sc.nextLine();
+            System.out.print("unidades: ");
+            int unidades = sc.nextInt();
+            sc.nextLine();
+            PreparedStatement ps = conn.prepareStatement("insert into producto_pedido (id_producto,id_pedido,unidades) values (?,?,?)");
+            ps.setInt(1,id_producto);
+            ps.setInt(2,id_pedido);
+            ps.setInt(3,unidades);
+            ps.executeUpdate();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        }
+
+        private void añadirPedidoMesa (){
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/resturante?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+            System.out.print("id_pedido: ");
+            int id_pedido = sc.nextInt();
+            sc.nextLine();
+            System.out.print("mesa: ");
+            int mesa = sc.nextInt();
+            sc.nextLine();
+            PreparedStatement ps = conn.prepareStatement("insert into pedido_mesa (id_pedido, mesa) values (?.?)");
+            ps.setInt(1,id_pedido);
+            ps.setInt(2,mesa);
+            ps.executeUpdate();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
 
         }
 
         private void salir () {
+
+        }
+
+        private void crearMesa (){
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/resturante?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root", "");
+            System.out.print("Crea numero de mesa: ");
+            int numero = sc.nextInt();
+            sc.nextLine();
+            System.out.print("¿Ocupada?: ");
+            int ocupada = sc.nextInt();
+            sc.nextLine();
+            PreparedStatement ps = conn.prepareStatement("insert into mesa (numero, ocupada) values (?,?)");
+            ps.setInt(1,numero);
+            ps.setInt(2,ocupada);
+            ps.executeUpdate();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
         }
     }
